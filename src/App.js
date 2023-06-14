d9da11e1e34a422c64d1cb2a407d950b6ccfd3e5
yeongmins <oxox123456@naver.com>
2023-06-14 17:22:12 +0900

    커밋

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    { title: "Car Insurance", amount: 294.67, date: new Date(2021, 2, 28) },
+    { title: "Car Insurance", amount: 294.67, date: new Date(2021, 2, 28) },
+    { title: "Car Insurance", amount: 294.67, date: new Date(2021, 2, 28) },
+    { title: "Car Insurance", amount: 294.67, date: new Date(2021, 2, 28) },
+  ];
+
   return (
     <div>
       <h2>Let's get started!</h2>
+      <ExpenseItem></ExpenseItem>
+      <ExpenseItem></ExpenseItem>
+      <ExpenseItem></ExpenseItem>
       <ExpenseItem></ExpenseItem>
     </div>
   );
 }
 
 export default App;
- 
